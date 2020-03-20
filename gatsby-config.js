d83b8e9abd1a44a17f1b21d4468ac062c2103ac8
Shaun Axani <shaunaxani@Shauns-iMac-9.local>
2020-03-19 21:30:21 -0700

    adding heroku cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "https://cole-cms.herokuapp.com/",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "project",
